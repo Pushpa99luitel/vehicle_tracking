2fea43c10c961fb138e68e9daf9b30985e7c31f5
Pushpa99luitel <luitelpushpa585@gmail.com>
2021-07-10 16:56:03 +0545

    added splash screen

--- app/src/main/java/com/pushpa/vehicletracking/SplashActivity.kt
+++ app/src/main/java/com/pushpa/vehicletracking/SplashActivity.kt
@@ -1,11 +1,26 @@
 package com.pushpa.vehicletracking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(3200)
+            startActivity(
+                Intent(
+                    this@SplashActivity,
+                    LoginActivity::class.java
+                )
+            )
+            finish()
+        }
     }
 }
