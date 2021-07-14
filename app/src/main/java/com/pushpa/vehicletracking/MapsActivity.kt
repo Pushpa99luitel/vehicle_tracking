c6ed40c9a5306aef727077df2f754b911696ba02
Pushpa99luitel <luitelpushpa585@gmail.com>
2021-07-14 19:34:21 +0545

    added location service

--- app/src/main/java/com/pushpa/vehicletracking/MapsActivity.kt
+++ app/src/main/java/com/pushpa/vehicletracking/MapsActivity.kt
@@ -1,7 +1,11 @@
 package com.pushpa.vehicletracking
 
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,14 +13,23 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class   MapsActivity : AppCompatActivity(), OnMapReadyCallback,LocationListener {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var reference:DatabaseReference
+    private lateinit var manager: LocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        reference=FirebaseDatabase.getInstance().getReference().child("User-101")
+        
+
+        //FirebaseDatabase.getInstance().getReference().setValue("Pushpa Luitel")
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -39,5 +52,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onLocationChanged(location: Location) {
+        if(location !=null){
+            saveLocation(location)
+        }else{
+            Toast.makeText(this, "No location", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun saveLocation(location: Location) {
+        reference.setValue(location)
+
     }
 }
